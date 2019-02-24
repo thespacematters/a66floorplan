88f64056fd9cda3bd4a324ddf75777233fcaeb85
brigittaforrai <forraibrigi@gmail.com>
2019-02-24 22:48:42 +0100

    floor names

--- index.js
+++ index.js
@@ -3,31 +3,43 @@
   let plans = document.querySelector('.plans')
   let sliderIndex = 0
   let floorList = []
+  let prev = document.querySelector('button[name="prev-btn"]')
+  let next = document.querySelector('button[name="next-btn"]')
+
   const FLOORS = {
-    0: 'basement',
-    1: 'ground floor',
-    2: '1st floor',
-    3: '2nd floor',
-    4: '3rd floor',
-    5: 'attic'
+    1: 'B',
+    2: '0',
+    3: '1',
+    4: '2',
+    5: '3',
+    6: 'A'
   }
 
   let layers = getParam('layers')
   filter()
   draw()
-  addEvent()
+  floorList.length > 1 ? addEvent() : hideButtons()
   slider(sliderIndex)
 
   function getParam (name) {
     name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
     var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
     var results = regex.exec(location.search);
-    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+    return results === null ? ['BG1A', 'BG2A', 'BG3A', 'BG4A', 'BG5A', 'W1A', 'W2A', 'W3A', 'W4A', 'W5A'] : decodeURIComponent(results[1].replace(/\+/g, ' '));
   }
 
   function addEvent () {
-    let prev = document.querySelector('button[name="prev-btn"]')
-    let next = document.querySelector('button[name="next-btn"]')
+    let cont = document.querySelector('.floor-btns')
+
+    Object.keys(filtered).forEach((floor, i) => {
+      let button = document.createElement('button')
+      button.classList.add('floor-name', `floor-${floor}`)
+      button.innerHTML = FLOORS[floor]
+      cont.appendChild(button)
+      button.addEventListener('click', () => {
+        slider(i)
+      })
+    })
 
     next.addEventListener('click', () => {
       slider(sliderIndex + 1)
@@ -37,12 +49,25 @@
     })
   }
 
+  function hideButtons () {
+    next.style.display = 'none'
+    prev.style.display = 'none'
+
+    let cont = document.querySelector('.floor-btns')
+    let button = document.createElement('button')
+    button.classList.add('floor-name', 'active')
+    button.innerHTML = FLOORS[Object.keys(filtered)[0]] || ''
+    cont.appendChild(button)
+  }
+
   function slider (index) {
     let length = floorList.length - 1
     sliderIndex = index < 0 ? length : index > length ? 0 : index
+    let buttons = document.querySelectorAll('.floor-name')
 
     floorList.forEach((f, i) => {
       f.style.display = i === sliderIndex ? 'block' : 'none'
+      i === sliderIndex ? buttons[i].classList.add('active') : buttons[i].classList.remove('active')
     })
   }
 
