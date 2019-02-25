5b529d89f011d5a7919c93ff70e93f15997787e6
Zsolt Bako <zsolt.bako@gmail.com>
2019-02-25 02:57:52 +0100

    Fixed reference file

--- reference.js
+++ reference.js
@@ -85,638 +85,638 @@ const REF = [{
   floor: 6
 }, {
   id: 'R1A',
-  name: 'R201A-2016-01-01',
+  name: 'R201A-2016-01-01.png',
   floor: 4
 }, {
   id: 'R2A',
-  name: 'R202-1A-2016-05-01',
+  name: 'R202-1A-2016-05-01.png',
   floor: 4
 }, {
   id: 'R3A',
-  name: 'R202-2A-2016-05-01',
+  name: 'R202-2A-2016-05-01.png',
   floor: 4
 }, {
   id: 'R4A',
-  name: 'R202-3A-2016-05-01',
+  name: 'R202-3A-2016-05-01.png',
   floor: 4
 }, {
   id: 'R5A',
-  name: 'R202-4A-2016-05-01',
+  name: 'R202-4A-2016-05-01.png',
   floor: 4
 }, {
   id: 'R6A',
-  name: 'R203-1A-2016-05-01',
+  name: 'R203-1A-2016-05-01.png',
   floor: 4
 }, {
   id: 'R7A',
-  name: 'R203-2A-2016-05-01',
+  name: 'R203-2A-2016-05-01.png',
   floor: 4
 }, {
   id: 'R178A',
-  name: 'R203-1A-2016-08-01',
+  name: 'R203-1A-2016-08-01.png',
   floor: 4
 }, {
   id: 'R184A',
-  name: 'R203-1A-2017-04-01',
+  name: 'R203-1A-2017-04-01.png',
   floor: 4
 }, {
   id: 'R186A',
-  name: 'R203-1A-2017-11-24',
+  name: 'R203-1A-2017-11-24.png',
   floor: 4
 }, {
   id: 'R185A',
-  name: 'R203-3A-2017-04-01',
+  name: 'R203-3A-2017-04-01.png',
   floor: 4
 }, {
   id: 'R179A',
-  name: 'R203-2A-2016-08-01',
+  name: 'R203-2A-2016-08-01.png',
   floor: 4
 }, {
   id: 'R176A',
-  name: 'R203A-2016-01-01',
+  name: 'R203A-2016-01-01.png',
   floor: 4
 }, {
   id: 'R8A',
-  name: 'R206A-2016-01-01',
+  name: 'R206A-2016-01-01.png',
   floor: 4
 }, {
   id: 'R9A',
-  name: 'R207A-2016-01-01',
+  name: 'R207A-2016-01-01.png',
   floor: 4
 }, {
   id: 'R10A',
-  name: 'R208A-2016-01-01',
+  name: 'R208A-2016-01-01.png',
   floor: 4
 }, {
   id: 'R11A',
-  name: 'R211-1A-2016-08-01',
+  name: 'R211-1A-2016-08-01.png',
   floor: 4
 }, {
   id: 'R12A',
-  name: 'R211-2A-2016-08-01',
+  name: 'R211-2A-2016-08-01.png',
   floor: 4
 }, {
   id: 'R196A',
-  name: 'R211A-2016-01-01',
+  name: 'R211A-2016-01-01.png',
   floor: 4
 }, {
   id: 'R13A',
-  name: 'R212-2A-2016-05-01',
+  name: 'R212-2A-2016-05-01.png',
   floor: 4
 }, {
   id: 'R187A',
-  name: 'R212-1A-2017-11-24',
+  name: 'R212-1A-2017-11-24.png',
   floor: 4
 }, {
   id: 'R188A',
-  name: 'R212-2A-2017-11-24',
+  name: 'R212-2A-2017-11-24.png',
   floor: 4
 }, {
   id: 'R180A',
-  name: 'R212A-2016-08-01',
+  name: 'R212A-2016-08-01.png',
   floor: 4
 }, {
   id: 'R177A',
-  name: 'R212A-2016-01-01',
+  name: 'R212A-2016-01-01.png',
   floor: 4
 }, {
   id: 'R14A',
-  name: 'R212-1A-2016-05-01',
+  name: 'R212-1A-2016-05-01.png',
   floor: 4
 }, {
   id: 'R15A',
-  name: 'R213A-2016-01-01',
+  name: 'R213A-2016-01-01.png',
   floor: 4
 }, {
   id: 'R16A',
-  name: 'R214-1A-2016-08-01',
+  name: 'R214-1A-2016-08-01.png',
   floor: 4
 }, {
   id: 'R182A',
-  name: 'R214-2A-2016-08-01',
+  name: 'R214-2A-2016-08-01.png',
   floor: 4
 }, {
   id: 'R17A',
-  name: 'R214-2A-2017-07-01',
+  name: 'R214-2A-2017-07-01.png',
   floor: 4
 }, {
   id: 'R18A',
-  name: 'R214-3A-2017-07-01',
+  name: 'R214-3A-2017-07-01.png',
   floor: 4
 }, {
   id: 'R183A',
-  name: 'R214-3A-2016-08-01',
+  name: 'R214-3A-2016-08-01.png',
   floor: 4
 }, {
   id: 'R181A',
-  name: 'R214A-2016-01-01',
+  name: 'R214A-2016-01-01.png',
   floor: 4
 }, {
   id: 'R19A',
-  name: 'R215A-2016-01-01',
+  name: 'R215A-2016-01-01.png',
   floor: 4
 }, {
   id: 'R20A',
-  name: 'R216A-2016-01-01',
+  name: 'R216A-2016-01-01.png',
   floor: 4
 }, {
   id: 'R21A',
-  name: 'R217A-2016-01-01',
+  name: 'R217A-2016-01-01.png',
   floor: 4
 }, {
   id: 'R22A',
-  name: 'R218A-2016-01-01',
+  name: 'R218A-2016-01-01.png',
   floor: 4
 }, {
   id: 'R23A',
-  name: 'R219A-2016-01-01',
+  name: 'R219A-2016-01-01.png',
   floor: 4
 }, {
   id: 'R24A',
-  name: 'R220A-2016-01-01',
+  name: 'R220A-2016-01-01.png',
   floor: 4
 }, {
   id: 'R25A',
-  name: 'R221A-2016-01-01',
+  name: 'R221A-2016-01-01.png',
   floor: 4
 }, {
   id: 'R26A',
-  name: 'R222A-2016-01-01',
+  name: 'R222A-2016-01-01.png',
   floor: 4
 }, {
   id: 'R27A',
-  name: 'R223A-2016-01-01',
+  name: 'R223A-2016-01-01.png',
   floor: 4
 }, {
   id: 'R28A',
-  name: 'R224A-2016-01-01',
+  name: 'R224A-2016-01-01.png',
   floor: 4
 }, {
   id: 'R29A',
-  name: 'R225A-2016-01-01',
+  name: 'R225A-2016-01-01.png',
   floor: 4
 }, {
   id: 'R175A',
-  name: 'R202A-2016-01-01',
+  name: 'R202A-2016-01-01.png',
   floor: 4
 }, {
   id: 'R30A',
-  name: 'R301A-2016-01-01',
+  name: 'R301A-2016-01-01.png',
   floor: 5
 }, {
   id: 'R31A',
-  name: 'R302A-2016-01-01',
+  name: 'R302A-2016-01-01.png',
   floor: 5
 }, {
   id: 'R32A',
-  name: 'R303-1A-2016-08-01',
+  name: 'R303-1A-2016-08-01.png',
   floor: 5
 }, {
   id: 'R33A',
-  name: 'R303-2A-2016-08-01',
+  name: 'R303-2A-2016-08-01.png',
   floor: 5
 }, {
   id: 'R34A',
-  name: 'R303-3A-2016-08-01',
+  name: 'R303-3A-2016-08-01.png',
   floor: 5
 }, {
   id: 'R35A',
-  name: 'R303-4A-2016-08-01',
+  name: 'R303-4A-2016-08-01.png',
   floor: 5
 }, {
   id: 'R36A',
-  name: 'R303-5A-2016-08-01',
+  name: 'R303-5A-2016-08-01.png',
   floor: 5
 }, {
   id: 'R189A',
-  name: 'R303A-2016-01-01',
+  name: 'R303A-2016-01-01.png',
   floor: 5
 }, {
   id: 'R37A',
-  name: 'R306A-2016-01-01',
+  name: 'R306A-2016-01-01.png',
   floor: 5
 }, {
   id: 'R38A',
-  name: 'R307A-2016-01-01',
+  name: 'R307A-2016-01-01.png',
   floor: 5
 }, {
   id: 'R39A',
-  name: 'R308A-2016-01-01',
+  name: 'R308A-2016-01-01.png',
   floor: 5
 }, {
   id: 'R40A',
-  name: 'R311A-2016-01-01',
+  name: 'R311A-2016-01-01.png',
   floor: 5
 }, {
   id: 'R41A',
-  name: 'R312-1A-2016-08-01',
+  name: 'R312-1A-2016-08-01.png',
   floor: 5
 }, {
   id: 'R42A',
-  name: 'R312-2A-2016-08-01',
+  name: 'R312-2A-2016-08-01.png',
   floor: 5
 }, {
   id: 'R190A',
-  name: 'R312A-2016-01-01',
+  name: 'R312A-2016-01-01.png',
   floor: 5
 }, {
   id: 'R43A',
-  name: 'R313-1A-2016-08-01',
+  name: 'R313-1A-2016-08-01.png',
   floor: 5
 }, {
   id: 'R44A',
-  name: 'R313-2A-2016-08-01',
+  name: 'R313-2A-2016-08-01.png',
   floor: 5
 }, {
   id: 'R191A',
-  name: 'R313A-2016-01-01',
+  name: 'R313A-2016-01-01.png',
   floor: 5
 }, {
   id: 'R45A',
-  name: 'R314-1A-2016-08-01',
+  name: 'R314-1A-2016-08-01.png',
   floor: 5
 }, {
   id: 'R46A',
-  name: 'R314-2A-2017-07-01',
+  name: 'R314-2A-2017-07-01.png',
   floor: 5
 }, {
   id: 'R194A',
-  name: 'R314-2A-2016-08-01',
+  name: 'R314-2A-2016-08-01.png',
   floor: 5
 }, {
   id: 'R195A',
-  name: 'R314-3A-2016-08-01',
+  name: 'R314-3A-2016-08-01.png',
   floor: 5
 }, {
   id: 'R192A',
-  name: 'R314A-2016-01-01',
+  name: 'R314A-2016-01-01.png',
   floor: 5
 }, {
   id: 'R54A',
-  name: 'R315A-2016-01-01',
+  name: 'R315A-2016-01-01.png',
   floor: 5
 }, {
   id: 'R55A',
-  name: 'R316A-2016-01-01',
+  name: 'R316A-2016-01-01.png',
   floor: 5
 }, {
   id: 'R56A',
-  name: 'R317A-2016-01-01',
+  name: 'R317A-2016-01-01.png',
   floor: 5
 }, {
   id: 'R57A',
-  name: 'R318A-2016-01-01',
+  name: 'R318A-2016-01-01.png',
   floor: 5
 }, {
   id: 'R58A',
-  name: 'R319A-2016-01-01',
+  name: 'R319A-2016-01-01.png',
   floor: 5
 }, {
   id: 'R59A',
-  name: 'R320A-2016-01-01',
+  name: 'R320A-2016-01-01.png',
   floor: 5
 }, {
   id: 'R60A',
-  name: 'R321A-2016-01-01',
+  name: 'R321A-2016-01-01.png',
   floor: 5
 }, {
   id: 'R61A',
-  name: 'R322A-2016-01-01',
+  name: 'R322A-2016-01-01.png',
   floor: 5
 }, {
   id: 'R62A',
-  name: 'R323A-2016-01-01',
+  name: 'R323A-2016-01-01.png',
   floor: 5
 }, {
   id: 'R63A',
-  name: 'R324A-2016-01-01',
+  name: 'R324A-2016-01-01.png',
   floor: 5
 }, {
   id: 'R64A',
-  name: 'R325A-2016-01-01',
+  name: 'R325A-2016-01-01.png',
   floor: 5
 }, {
   id: 'R68A',
-  name: 'RP01A-2016-01-01',
+  name: 'RP01A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R69A',
-  name: 'RP02A-2016-01-01',
+  name: 'RP02A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R70A',
-  name: 'RP03A-2016-01-01',
+  name: 'RP03A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R71A',
-  name: 'RP04A-2016-01-01',
+  name: 'RP04A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R72A',
-  name: 'RP05A-2016-01-01',
+  name: 'RP05A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R73A',
-  name: 'RP06A-2016-01-01',
+  name: 'RP06A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R74A',
-  name: 'RP07A-2016-01-01',
+  name: 'RP07A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R75A',
-  name: 'RP08A-2016-01-01',
+  name: 'RP08A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R166A',
-  name: 'RP09A-2016-01-01',
+  name: 'RP09A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R167A',
-  name: 'RP10A-2016-01-01',
+  name: 'RP10A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R83A',
-  name: 'RP16A-2016-01-01',
+  name: 'RP16A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R84A',
-  name: 'RP17A-2016-01-01',
+  name: 'RP17A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R85A',
-  name: 'RP18A-2016-01-01',
+  name: 'RP18A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R86A',
-  name: 'RP19A-2016-01-01',
+  name: 'RP19A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R87A',
-  name: 'RP20A-2016-01-01',
+  name: 'RP20A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R88A',
-  name: 'RP21A-2016-01-01',
+  name: 'RP21A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R89A',
-  name: 'RP22A-2016-01-01',
+  name: 'RP22A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R90A',
-  name: 'RP23A-2016-01-01',
+  name: 'RP23A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R91A',
-  name: 'RP24A-2016-01-01',
+  name: 'RP24A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R92A',
-  name: 'RP25A-2016-01-01',
+  name: 'RP25A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R93A',
-  name: 'RP26A-2016-01-01',
+  name: 'RP26A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R95A',
-  name: 'RP28A-2016-01-01',
+  name: 'RP28A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R96A',
-  name: 'RP29A-2016-01-01',
+  name: 'RP29A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R97A',
-  name: 'RP30A-2016-01-01',
+  name: 'RP30A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R98A',
-  name: 'RP31A-2016-01-01',
+  name: 'RP31A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R99A',
-  name: 'RP32A-2016-01-01',
+  name: 'RP32A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R100A',
-  name: 'RP33A-2016-01-01',
+  name: 'RP33A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R101A',
-  name: 'RP34A-2016-01-01',
+  name: 'RP34A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R102A',
-  name: 'RP35A-2016-01-01',
+  name: 'RP35A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R168A',
-  name: 'RP36A-2016-01-01',
+  name: 'RP36A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R104A',
-  name: 'RP37A-2016-01-01',
+  name: 'RP37A-2016-01-01.png',
   floor: 1
 }, {
   id: 'R105A',
-  name: 'R001A-2016-01-01',
+  name: 'R001A-2016-01-01.png',
   floor: 2
 }, {
   id: 'R106A',
-  name: 'R002A-2016-01-01',
+  name: 'R002A-2016-01-01.png',
   floor: 2
 }, {
   id: 'R107A',
-  name: 'R003A-2016-01-01',
+  name: 'R003A-2016-01-01.png',
   floor: 2
 }, {
   id: 'R110A',
-  name: 'R006A-2016-01-01',
+  name: 'R006A-2016-01-01.png',
   floor: 2
 }, {
   id: 'R111A',
-  name: 'R007A-2016-01-01',
+  name: 'R007A-2016-01-01.png',
   floor: 2
 }, {
   id: 'R112A',
-  name: 'R008A-2016-01-01',
+  name: 'R008A-2016-01-01.png',
   floor: 2
 }, {
   id: 'R113A',
-  name: 'R009-1A-2016-01-01',
+  name: 'R009-1A-2016-01-01.png',
   floor: 2
 }, {
   id: 'R170A',
-  name: 'R009-1A-2019-04-01',
+  name: 'R009-1A-2019-04-01.png',
   floor: 2
 }, {
   id: 'R172A',
-  name: 'R009-1A-2016-05-01',
+  name: 'R009-1A-2016-05-01.png',
   floor: 2
 }, {
   id: 'R136A',
-  name: 'R009-2A-2016-01-01',
+  name: 'R009-2A-2016-01-01.png',
   floor: 2
 }, {
   id: 'R171A',
-  name: 'R009-2A-2019-04-01',
+  name: 'R009-2A-2019-04-01.png',
   floor: 2
 }, {
   id: 'R173A',
-  name: 'R009-2A-2016-05-01',
+  name: 'R009-2A-2016-05-01.png',
   floor: 2
 }, {
   id: 'R137A',
-  name: 'R009-3A-2016-01-01',
+  name: 'R009-3A-2016-01-01.png',
   floor: 2
 }, {
   id: 'R174A',
-  name: 'R009-3A-2016-05-01',
+  name: 'R009-3A-2016-05-01.png',
   floor: 2
 }, {
   id: 'R114A',
-  name: 'R010A-2016-01-01',
+  name: 'R010A-2016-01-01.png',
   floor: 2
 }, {
   id: 'R169A',
-  name: 'R010A-2019-04-01',
+  name: 'R010A-2019-04-01.png',
   floor: 2
 }, {
   id: 'R115A',
-  name: 'R011A-2016-01-01',
+  name: 'R011A-2016-01-01.png',
   floor: 2
 }, {
   id: 'R126A',
-  name: 'R022A-2016-01-01',
+  name: 'R022A-2016-01-01.png',
   floor: 2
 }, {
   id: 'R127A',
-  name: 'R023A-2016-01-01',
+  name: 'R023A-2016-01-01.png',
   floor: 2
 }, {
   id: 'R128A',
-  name: 'R024A-2016-01-01',
+  name: 'R024A-2016-01-01.png',
   floor: 2
 }, {
   id: 'R129A',
-  name: 'R025A-2016-01-01',
+  name: 'R025A-2016-01-01.png',
   floor: 2
 }, {
   id: 'R130A',
-  name: 'R026A-2016-01-01',
+  name: 'R026A-2016-01-01.png',
   floor: 2
 }, {
   id: 'R131A',
-  name: 'R027A-2016-01-01',
+  name: 'R027A-2016-01-01.png',
   floor: 2
 }, {
   id: 'R132A',
-  name: 'R028A-2016-01-01',
+  name: 'R028A-2016-01-01.png',
   floor: 2
 }, {
   id: 'R133A',
-  name: 'R029A-2016-01-01',
+  name: 'R029A-2016-01-01.png',
   floor: 2
 }, {
   id: 'R135A',
-  name: 'R031A-2016-01-01',
+  name: 'R031A-2016-01-01.png',
   floor: 2
 }, {
   id: 'R138A',
-  name: 'R101A-2016-01-01',
+  name: 'R101A-2016-01-01.png',
   floor: 3
 }, {
   id: 'R139A',
-  name: 'R102-1A-2016-01-01',
+  name: 'R102-1A-2016-01-01.png',
   floor: 3
 }, {
   id: 'R159A',
-  name: 'R102-2A-2016-01-01',
+  name: 'R102-2A-2016-01-01.png',
   floor: 3
 }, {
   id: 'R140A',
-  name: 'R103A-2016-01-01',
+  name: 'R103A-2016-01-01.png',
   floor: 3
 }, {
   id: 'R143A',
-  name: 'R106A-2016-01-01',
+  name: 'R106A-2016-01-01.png',
   floor: 3
 }, {
   id: 'R144A',
-  name: 'R107A-2016-01-01',
+  name: 'R107A-2016-01-01.png',
   floor: 3
 }, {
   id: 'R145A',
-  name: 'R108A-2016-01-01',
+  name: 'R108A-2016-01-01.png',
   floor: 3
 }, {
   id: 'R148A',
-  name: 'R111A-2016-01-01',
+  name: 'R111A-2016-01-01.png',
   floor: 3
 }, {
   id: 'R149A',
-  name: 'R112-1A-2016-01-01',
+  name: 'R112-1A-2016-01-01.png',
   floor: 3
 }, {
   id: 'R160A',
-  name: 'R112-2A-2016-01-01',
+  name: 'R112-2A-2016-01-01.png',
   floor: 3
 }, {
   id: 'R150A',
-  name: 'R113A-2016-01-01',
+  name: 'R113A-2016-01-01.png',
   floor: 3
 }, {
   id: 'R151A',
-  name: 'R114-1A-2016-01-01',
+  name: 'R114-1A-2016-01-01.png',
   floor: 3
 }, {
   id: 'R161A',
-  name: 'R114-2A-2016-01-01',
+  name: 'R114-2A-2016-01-01.png',
   floor: 3
 }, {
   id: 'R152A',
-  name: 'R115A-2016-01-01',
+  name: 'R115A-2016-01-01.png',
   floor: 3
 }, {
   id: 'R153A',
-  name: 'R116A-2016-01-01',
+  name: 'R116A-2016-01-01.png',
   floor: 3
 }, {
   id: 'R154A',
-  name: 'R117A-2016-01-01',
+  name: 'R117A-2016-01-01.png',
   floor: 3
 }, {
   id: 'R155A',
-  name: 'R118A-2016-01-01',
+  name: 'R118A-2016-01-01.png',
   floor: 3
 }, {
   id: 'R156A',
-  name: 'R119A-2016-01-01',
+  name: 'R119A-2016-01-01.png',
   floor: 3
 }, {
   id: 'R157A',
-  name: 'R120A-2016-01-01',
+  name: 'R120A-2016-01-01.png',
   floor: 3
 }, {
   id: 'R158A',
-  name: 'R121A-2016-01-01',
+  name: 'R121A-2016-01-01.png',
   floor: 3
 }, {
   id: 'R162A',
-  name: 'R122A-2016-01-01',
+  name: 'R122A-2016-01-01.png',
   floor: 3
 }, {
   id: 'R163A',
-  name: 'R123A-2016-01-01',
+  name: 'R123A-2016-01-01.png',
   floor: 3
 }, {
   id: 'R164A',
-  name: 'R124A-2016-01-01',
+  name: 'R124A-2016-01-01.png',
   floor: 3
 }, {
   id: 'R165A',
-  name: 'R125A-2016-01-01',
+  name: 'R125A-2016-01-01.png',
   floor: 3
 }]
